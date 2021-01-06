45e17a4e186767d1dd423c2692a1a3c3a706c6d1
Rondinelli Morais <rondinellimorais@gmail.com>
2021-01-06 08:15:10 -0300

    fix build.sh

--- build.sh
+++ build.sh
@@ -10,8 +10,16 @@ python3 -m venv .venv
 # active virtual environment
 source .venv/bin/activate
 
+#
+# Workaround to the [#4919](https://github.com/scikit-image/scikit-image/issues/4919)
+# ModuleNotFoundError: No module named 'numpy'
+# when install scikit-image
+#
+# 06/01/2021
+python3 -m pip install numpy==1.19.4
+
 # install requirements
 python3 -m pip install -r requirements.txt
 
 # exit virtual environment
-# deactivate
+deactivate
