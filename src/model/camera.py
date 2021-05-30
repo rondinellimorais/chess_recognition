eea4601db903e94077bc0868583e36d7b0f63182
Rondinelli Morais <rondinellimorais@gmail.com>
2021-05-30 13:06:04 -0300

    melhorias no processo de deteccao das pecas

--- src/model/camera.py
+++ src/model/camera.py
@@ -1,6 +1,4 @@
-import numpy as np
 import cv2
-from threading import Timer
 import time
 
 class Camera:
--- src/model/game.py
+++ src/model/game.py
@@ -1,19 +1,23 @@
+from typing import Dict
 from model.chessboard_calibration import ChessboardCalibration
 from model.board import Board
 from model.camera import Camera
 from dotenv import dotenv_values
 import cv2
+import time
 
 class Game:
   fps: int
   cam_address: str
   running_calibration: ChessboardCalibration
   board: Board
+  previousMillis: int = 0
+  __config: Dict
 
   def __init__(self):
-    config = dotenv_values()
-    self.fps = int(config.get('CAM_FPS'))
-    self.cam_address = config.get('CAM_ADDRESS')
+    self.__config = dotenv_values()
+    self.fps = int(self.__config.get('CAM_FPS'))
+    self.cam_address = self.__config.get('CAM_ADDRESS')
 
   def mapping(self):
     """
@@ -55,7 +59,14 @@ class Game:
     """
     img = self.running_calibration.applyMapping(frame)
 
-    # self.board.state(img)
+    currentMillis = round(time.time() * 1000)
+
+    # check to see if it's time to running vision computer; that is, if the difference
+    # between the current time and last time we running vision computer is bigger than
+    # the interval at which we want to running vision computer.
+    if currentMillis - self.previousMillis >= int(self.__config.get('CHECK_BOARD_STATE_INTERVAL')):
+      self.previousMillis = currentMillis
+      self.board.state(img)
 
     cv2.imshow("preview", img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
