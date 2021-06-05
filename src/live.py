727743cae133c2efe0dfec752c57d58a49c93463
Rondinelli Morais <rondinellimorais@gmail.com>
2021-06-05 15:58:55 -0300

    criacao do grid

--- src/live.py
+++ src/live.py
@@ -12,21 +12,23 @@ from model.chessboard_calibration import ChessboardCalibration
 # pyqtgraph.examples.run()
 
 class App(QtGui.QMainWindow):
+  __canvas: pg.GraphicsLayoutWidget = None
+
   def __init__(self, parent=None):
     super(App, self).__init__(parent)
 
     #### Create Gui Elements ###########
-    self.canvas = pg.GraphicsLayoutWidget(size=(540, 540))
-    self.canvas.setWindowTitle('pyqtgraph example: ImageItem')
-    self.canvas.ci.layout.setContentsMargins(0, 0, 0, 0)
-    
-    self.view = self.canvas.addViewBox()
-    self.view.setBackgroundColor('r')
-    self.view.suggestPadding = lambda *_: 0.0
-    self.view.installEventFilter(self)
+    self.__canvas = pg.GraphicsLayoutWidget(size=(500 * 2, 500))
+    self.__canvas.setWindowTitle('pyqtgraph example: ImageItem')
+    self.__canvas.ci.layout.setContentsMargins(0, 0, 0, 0)
 
-    self.img = pg.ImageItem()
-    self.view.addItem(self.img)
+    ## add grid
+    self.imgs:list[pg.ImageItem] = []
+    views = self.__addGridLayout(numrows=1, numcols=2)
+    for view in views:
+      imgItem = pg.ImageItem()
+      self.imgs.append(imgItem)
+      view.addItem(imgItem)
 
     #### Set Data  #####################
     self.counter = 0
@@ -37,24 +39,39 @@ class App(QtGui.QMainWindow):
     self.camera = Camera('http://192.168.0.111:4747/video', fps=30)
     self.__running_calibration = ChessboardCalibration()
     found, self.board = self.__running_calibration.loadMapping()
-    self._update()
+    self.__update()
+
+  def __addGridLayout(self, numrows: int, numcols: int) -> list[pg.ViewBox]:
+    views: list[pg.ViewBox] = []
+    for r in range(numrows):
+      for c in range(numcols):
+        view: pg.ViewBox = self.__canvas.addViewBox(row=r, col=c)
+        view.suggestPadding = lambda *_: 0.0
+        view.installEventFilter(self)
+        view.setBackgroundColor('r')
+        views.append(view)
+    return views
 
   def eventFilter(self, watched, event):
     if event.type() == QtCore.QEvent.GraphicsSceneWheel:
       return True
     return super().eventFilter(watched, event)
 
-  def show(self):
-    self.canvas.show()
+  def setImage(self, img):
+    for imgItem in self.imgs:
+      imgItem.setImage(img)
 
-  def _update(self):
+  def show(self):
+    self.__canvas.show()
+
+  def __update(self):
     frame = self.camera.capture()
     img = self.__running_calibration.applyMapping(frame)
     
-    self.board.scan(img)
+    # self.board.scan(img)
     
     rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    self.img.setImage(cv2.rotate(rgb_img, cv2.ROTATE_90_CLOCKWISE))
+    self.setImage(cv2.rotate(rgb_img, cv2.ROTATE_90_CLOCKWISE))
 
     now = time.time()
     dt = (now - self.lastupdate)
@@ -65,9 +82,8 @@ class App(QtGui.QMainWindow):
     self.fps = self.fps * 0.9 + fps2 * 0.1
     tx = 'Mean Frame Rate:  {:.3f} FPS'.format(self.fps)
     # self.label.setText(tx)
-    QtCore.QTimer.singleShot(1, self._update)
+    QtCore.QTimer.singleShot(1, self.__update)
     self.counter += 1
-
 
 if __name__ == '__main__':
   app = QtGui.QApplication(sys.argv)
